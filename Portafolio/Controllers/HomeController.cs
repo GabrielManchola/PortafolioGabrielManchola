b429a3a2503a3cd5b3a2df1b6b25075876bca6e7
GabrielManchola <50031687+GabrielManchola@users.noreply.github.com>
2022-01-13 18:28:04 -0500

    Agregar archivos de proyecto.

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var persona = new Persona()
+            {
+                Nombre = "Gabriel Manchola Arango",
+                Edad = 22,
+                Gustos = "Practicar y estudiar programacion, tocar guitarra, leer y viajar"
+            };
+
+            ViewBag.Edad = 22;
+            return View(persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
